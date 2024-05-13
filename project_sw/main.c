da550442ab06903e9aa494a61a7545e641a7053b
yl5334 <yl5334@columbia.edu>
2024-05-12 20:02:11 -0400

    bomb display

--- project_sw/main.c
+++ project_sw/main.c
@@ -504,20 +504,20 @@ void redrawTile(uint32_t x, uint32_t y) {
     
     switch (bomb_grid[x][y].type) {
 	case BOMB_TYPE_NORMAL:
-		bomb_coordinate = ((y << 10) | x);
-	if((!bomb_grid[x][y].used) && ((color.p1_state & 0x8) != 0x8)){
-		color.p1_bomb = bomb_coordinate;
-        color.p1_state |= 0x8;
-        bomb_grid[x][y].used = 1;
-        set_background_color(&color);
-	}
-	else if((!bomb_grid[x][y].used) && ((color.p2_state & 0x8) != 0x8))
-        {
-		color.p2_bomb = bomb_coordinate;
-        color.p2_state |= 0x8;
-        bomb_grid[x][y].used = 1;
-        set_background_color(&color);
-	}
+		bomb_coordinate = (((y << 4 - y) << 10) | (x << 4 - x));
+        if((!bomb_grid[x][y].used) && ((color.p1_state & 0x8) != 0x8)){
+            color.p1_bomb = bomb_coordinate;
+            color.p1_state |= 0x8;
+            bomb_grid[x][y].used = 1;
+            set_background_color(&color);
+        }
+        else if((!bomb_grid[x][y].used) && ((color.p2_state & 0x8) != 0x8))
+            {
+            color.p2_bomb = bomb_coordinate;
+            color.p2_state |= 0x8;
+            bomb_grid[x][y].used = 1;
+            set_background_color(&color);
+        }
         
 		break;
 	case BOMB_EMPTY:
